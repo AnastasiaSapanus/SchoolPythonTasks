fc3a3b5c05b1b9053bf64eb13588f29740b74e45
Anastasia Sapanus <anastasia.sapanus@gmail.com>
2020-05-27 23:35:01 +0300

    fix: calculation

--- friday13.py
+++ friday13.py
@@ -11,6 +11,7 @@ while year < 2011:
  day += 7
 
  limit = [0,31,28,31,30,31,30,31,31,30,31,30,31]
+
  if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
   limit[2] += 1
 
@@ -23,10 +24,20 @@ while year < 2011:
   month=1
 
  if day==13:
-   fridays13th[year]+=1
+  fridays13th[year]+=1
 
- for count in fridays13th:
-   count = fridays13th[year]
-begin,end = [int(s) for s in input().split()]
-count +=sum(fridays13th[begin : end + 1])
+#count = 0
+#for k in fridays13th:
+ #count += k
+
+count = 0
+
+k = int(input())
+
+for i in range(k):
+ begin,end = [int(s) for s in input().split()]
+ count +=sum(fridays13th[begin : end + 1])
+
+fridays13th.clear()
+
 print(count)
